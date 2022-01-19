9a95ab4faabbafbac7285ee3d1eaaf3be3d683e8
Khuirul-Huda <hudah2102@gmail.com>
2022-01-19 15:31:33 +0700

    added typing animation

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+jarallax(document.querySelectorAll('.jarallax'), {
+    speed: 0.2
+});
+
+var typed = new Typed('.herotext',{
+    strings:["We are good students", "We are great students"],
+    backSpeed: 80,
+    typeSpeed: 80,
+    loop: true,
+    shuffle: true
+  });
